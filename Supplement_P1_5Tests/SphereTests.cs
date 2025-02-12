059710339bb930710cc760b366a849a09d8dc7c5
donp8on <paytonadonis@gmail.com>
2025-02-12 15:59:59 -0500

    test: created test for Sphere class

---
+++ Supplement_P1_5Tests/SphereTests.cs
@@ -0,0 +1,22 @@
+﻿using Supplement_P1_5;
+using Xunit;
+
+namespace Supplement_P1_5Tests
+{
+    public class SphereTests
+    {
+        [Fact]
+        public void Sphere_ShouldCalcCorrectVolumeAndSurfaceAreaFor()
+        {
+            var sphere = new Sphere(3);
+            Assert.Equal(113.097, sphere.Volume(), 3);
+            Assert.Equal(113.097, sphere.SurfaceArea(), 3);
+        }
+
+        [Fact]
+        public void Sphere_ShouldThrowExceptionWhenRadiusIsInvalid()
+        {
+            Assert.Throws<ArgumentException>(() => new Sphere(-1));
+        }
+    }
+}
