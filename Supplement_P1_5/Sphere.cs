7f2dce4b5badbce820914e35e587e18aeeeed714
donp8on <paytonadonis@gmail.com>
2025-02-12 16:04:44 -0500

    doc: created documentation for Sphere class

--- Supplement_P1_5/Sphere.cs
+++ Supplement_P1_5/Sphere.cs
@@ -1,5 +1,10 @@
 ﻿namespace Supplement_P1_5
 {
+    /// <summary>
+    /// This is a 3D sphere shape.
+    /// The class provides functionality to calculate the sphere's volume and surface area.
+    /// It includes validation to ensure the radius is greater than zero.
+    /// </summary>
     public class Sphere : Shape3D
     {
         private double radius;
