c8fe318fa18dcdb462c5e85b426abefc0c9f3f5d
donp8on <paytonadonis@gmail.com>
2025-02-12 15:53:28 -0500

    feat: implemented Cube class inherited from Shape3D

---
+++ Supplement_P1_5/Cube.cs
@@ -0,0 +1,36 @@
+﻿using Supplement_P1_5;
+
+namespace Supplement_P1_5
+{
+    public class Cube : Shape3D
+    {
+        private double sideLength;
+        public Cube(double sideLength)
+        {
+            this.sideLength = sideLength;
+            Validate();
+        }
+
+        private void Validate()
+        {
+            if (sideLength <= 0)
+            {
+                throw new System.ArgumentException("Side length must be greater than 0.");
+            }
+        }
+
+        public override double Volume()
+        {
+            return sideLength * sideLength * sideLength;
+        }
+        public override double SurfaceArea()
+        {
+            return 6 * sideLength * sideLength;
+        }
+        public override string Dump()
+        {
+            return $"Cube: Surafce Area = {SurfaceArea():F2}, Volume:{Volume():F2}";
+        }
+    }
+}
+
