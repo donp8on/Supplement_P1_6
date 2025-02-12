6071328deb240880b5e91d883c4e44580df7ffc7
donp8on <paytonadonis@gmail.com>
2025-02-12 16:08:53 -0500

    test: created test for Cylinder class

---
+++ Supplement_P1_5Tests/CylinderTests.cs
@@ -0,0 +1,22 @@
+﻿using Supplement_P1_5;
+using Xunit;
+
+namespace Supplement_P1_5Tests
+{
+    public class CylinderTests
+    {
+        [Fact]
+        public void Cylinder_ShouldCalcCorrectVolumeAndSurfaceAreaFor()
+        {
+            var cylinder = new Cylinder(3, 5);
+            Assert.Equal(141.372, cylinder.Volume(), 3);
+            Assert.Equal(150.796, cylinder.SurfaceArea(), 3);
+        }
+
+        [Fact]
+        public void Cylinder_ShouldThrowExceptionWhenRadiusIsInvalid()
+        {
+            Assert.Throws<ArgumentException>(() => new Cylinder(-1, 4));
+        }
+    }
+}
