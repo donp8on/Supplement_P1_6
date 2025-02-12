0731d5f7bf2e610925fcd7380b3a9aa347c77c83
donp8on <paytonadonis@gmail.com>
2025-02-12 16:02:52 -0500

    feat: implemented Sphere class inherited from Shape3D

---
+++ Supplement_P1_5/Sphere.cs
@@ -0,0 +1,31 @@
+﻿namespace Supplement_P1_5
+{
+    public class Sphere : Shape3D
+    {
+        private double radius;
+        public Sphere(double radius)
+        {
+            this.radius = radius;
+            Validate();
+        }
+        private void Validate()
+        {
+            if (radius <= 0)
+            {
+                throw new System.ArgumentException("Radius must be greater than 0.");
+            }
+        }
+        public override double Volume()
+        {
+            return 4.0 / 3.0 * System.Math.PI * Math.Pow(radius, 3);
+        }
+        public override double SurfaceArea()
+        {
+            return 4 * System.Math.PI * radius * radius;
+        }
+        public override string Dump()
+        {
+            return $"Sphere: Surface Area = {SurfaceArea():F2}, Volume:{Volume():F2}";
+        }
+    }
+}
