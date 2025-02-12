23ace3f65e7322760ade1e52d4ed46d63373998c
donp8on <paytonadonis@gmail.com>
2025-02-12 15:45:33 -0500

    feat: implemented abstract class Shape3D

--- Supplement_P1_5/Shape3D.cs
+++ Supplement_P1_5/Shape3D.cs
@@ -2,6 +2,10 @@
 {
     public abstract class Shape3D
     {
+        public abstract double Volume();
 
+        public abstract double SurfaceArea();
+
+        public abstract string Dump();
     }
 }
