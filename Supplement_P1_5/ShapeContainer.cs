1b50dc5cda3e91343e5083906e3a1959812cd5f6
donp8on <paytonadonis@gmail.com>
2025-02-18 14:02:21 -0500

    feat: implemented ShapeContainer class providing create, retrieve, and delete shapes

---
+++ Supplement_P1_5/ShapeContainer.cs
@@ -0,0 +1,24 @@
+﻿using Supplement_P1_5;
+
+namespace Supplement_P1_5
+{
+    public class ShapeContainer
+    {
+        private List<Shape3D> shapes = new List<Shape3D>();
+
+        public void Create(Shape3D shape) => shapes.Add(shape);
+
+
+        public Shape3D Get(int index) => (index >= 0 && index < shapes.Count) ? shapes[index] : null;
+
+        public bool Delete(int index)
+        {
+            if (index >= 0 && index < shapes.Count)
+            {
+                shapes.RemoveAt(index);
+                return true;
+            }
+            return false;
+        }
+    }
+}
