1deceacce90cbc1de5e361e468c0a807ba454f74
donp8on <paytonadonis@gmail.com>
2025-02-18 14:36:04 -0500

    doc: documentated ShapeContainer class

--- Supplement_P1_5/ShapeContainer.cs
+++ Supplement_P1_5/ShapeContainer.cs
@@ -2,6 +2,10 @@
 
 namespace Supplement_P1_5
 {
+    /// <summary>
+    /// This class is a container for storing and managing 3D shapes for creating,
+    /// getting, and deleting shapes.
+    /// </summary>
     public class ShapeContainer
     {
         private List<Shape3D> shapes = new List<Shape3D>();
@@ -11,6 +15,7 @@ namespace Supplement_P1_5
 
         public Shape3D Get(int index) => (index >= 0 && index < shapes.Count) ? shapes[index] : null;
 
+    
         public bool Delete(int index)
         {
             if (index >= 0 && index < shapes.Count)
